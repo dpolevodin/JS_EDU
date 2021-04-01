273910080a13744989ce367f81eb93a52135f5bc
Dima <77278863+dpolevodin@users.noreply.github.com>
2021-04-01 15:50:40 +0500

    Update bubble_programm.js

--- bubble_programm.js
+++ bubble_programm.js
@@ -5,6 +5,14 @@ let score = [60, 50, 60, 58, 54, 54,
              46, 31, 57, 52, 44, 18,
              41, 53, 55, 61, 61, 44];
 
+let costs = [.25, .27, .25, .25, .25, .25,
+             .33, .31, .25, .29, .27, .22,
+             .21, .25, .25, .33, .21, .25,
+             .25, .25, .28, .25, .24, .22,
+             .20, .25, .30, .25, .24, .25,
+             .25, .25, .27, .25, .26, .29];
+
+
 const printAndGetHighScore = array => {
     let highScore = 0;
     let output;
@@ -28,6 +36,21 @@ const getBestResult = (array, highScore) => {
     return maxValue;
 }
 
+const getMostCostEffect = (array, costs, highScore) => {
+    let cost = 100;
+    let index;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == highScore) {
+            if (cost > costs[i]) {
+                index = i;
+                cost = costs[i];
+            }
+        }
+    }
+    return index;
+}
+
 let highScoreTest = printAndGetHighScore(score);
 console.log("Bubbles tests: " + score.length);
 console.log("Bubbles max value: " + highScoreTest);
@@ -35,3 +58,6 @@ console.log("Bubbles max value: " + highScoreTest);
 
 let bestSolutions = getBestResult(score, highScoreTest);
 console.log("Solution with highest score: " + bestSolutions);
+
+let mostEffVal = getMostCostEffect(score, costs, highScoreTest);
+console.log('Bubble solution # ' + mostEffVal + ' is the most cost effective');
