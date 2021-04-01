2373493c6b110370d7a8ee28a51e3120970878d6
Dima <77278863+dpolevodin@users.noreply.github.com>
2021-04-01 14:23:33 +0500

    Update bubble_programm.js

--- bubble_programm.js
+++ bubble_programm.js
@@ -1,27 +1,37 @@
-let array = [60, 50, 60, 58, 54, 54,
+let score = [60, 50, 60, 58, 54, 54,
              58, 50, 52, 54, 48, 69,
              34, 55, 51, 52, 44, 51,
              69, 64, 66, 55, 52, 61,
              46, 31, 57, 52, 44, 18,
              41, 53, 55, 61, 61, 44];
 
-let highScore = 0;
-
-for (let i = 0; i < array.length; i ++) {
-    let output = 'Bubble solution #' + i + ' score: ' + array[i];
-    console.log(output);
-    if (array[i] > highScore) {
-        highScore = array[i];
+const printAndGetHighScore = array => {
+    let highScore = 0;
+    let output;
+    for (let i = 0; i < array.length; i ++) {
+        let output = 'Bubble solution #' + i + ' score: ' + array[i];
+        console.log(output);
+        if (array[i] > highScore) {
+            highScore = array[i];
+        }
     }
+    return highScore;
 }
 
-let maxValue = [];
-for (let i=0; i < array.length; i++) {
-    if (array[i] == highScore) {
-        maxValue.push(i);
+const getBestResult = (array, highScore) => {
+    let maxValue = [];
+    for (let i=0; i < array.length; i++) {
+        if (array[i] == highScore) {
+            maxValue.push(i);
+        }
     }
+    return maxValue;
 }
 
-console.log("Bubbles tests: " + array.length);
-console.log("Bubbles max value: " + highScore);
-console.log("Solution with highest score: " + maxValue);
+let highScoreTest = printAndGetHighScore(score);
+console.log("Bubbles tests: " + score.length);
+console.log("Bubbles max value: " + highScoreTest);
+
+
+let bestSolutions = getBestResult(score, highScoreTest);
+console.log("Solution with highest score: " + bestSolutions);
