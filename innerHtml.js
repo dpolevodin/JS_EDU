851177eff043181de96e6c1f2da0a6e8c2d50d11
Dima <77278863+dpolevodin@users.noreply.github.com>
2021-04-12 22:43:25 +0500

    Update and rename innerHtml.js to DOM_innerHtml.html

--- innerHtml.js
+++
@@ -1,4 +0,0 @@
-let planet = document.getElementById('cod2');
-planet.innerHTML = 'Change element cod2';
-
-//возможны проблемы с загрузкой страницы. Либо надо дождаться загрузки страницы до запуска скрипта, либо переместить код в самый низ
