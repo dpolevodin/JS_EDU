94615c7e7d2dff46c1ec5be71b91c1a4733a3fa2
Dima <77278863+dpolevodin@users.noreply.github.com>
2021-04-12 22:34:30 +0500

    Create innerHtml.js

---
+++ innerHtml.js
@@ -0,0 +1,4 @@
+let planet = document.getElementById('cod2');
+planet.innerHTML = 'Change element cod2';
+
+//возможны проблемы с загрузкой страницы. Либо надо дождаться загрузки страницы до запуска скрипта, либо переместить код в самый низ
