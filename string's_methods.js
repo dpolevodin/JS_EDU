9a21587daed3dafba5a26005628aee171f74ee0d
Dima <77278863+dpolevodin@users.noreply.github.com>
2021-04-25 17:25:55 +0500

    Create string's_methods.js

---
+++ string's_methods.js
@@ -0,0 +1,11 @@
+let phrase ='test string for test';
+let index = phrase.indexOf('string');
+console.log(index); // '5'
+
+let phrase2 ='test string for test';
+let index2 = phrase.substring(5, 11);
+console.log(index2); // 'string'
+
+let phrase3 ='test string for test';
+let index3 = phrase.split(' ');
+console.log(index3); // ["test","string","for","test"]
