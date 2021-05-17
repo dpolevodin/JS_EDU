fc7869ee0c7bdc8f68279ebdf198ba9ce3ab2a24
Dima <77278863+dpolevodin@users.noreply.github.com>
2021-05-17 16:39:53 +0500

    Update array_sort.js

--- array_sort.js
+++ array_sort.js
@@ -12,12 +12,43 @@ let products = [ { name: "Grapefruit", calories: 170, color: "red", sold: 8200 }
 function compareSold(colaA, colaB) {
     if (colaA.sold > colaB.sold) {
         return 1;
-    } else if (colaA === colaB) {
+    } else if (colaA.sold === colaB.sold) {
         return 0;
     } else {
         return -1;
     }
 }
+
+function compareName(colaA, colaB) {
+    if (colaA.name > colaB.name) {
+        return 1;
+    } else if (colaA.name === colaB.name) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+function compareCalories(colaA, colaB) {
+    if (colaA.calories > colaB.calories) {
+        return 1;
+    } else if (colaA.calories === colaB.calories) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+function compareColor(colaA, colaB) {
+    if (colaA.color > colaB.color) {
+        return 1;
+    } else if (colaA.color === colaB.color) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
     
 function printProducts(products) {
     for (let i = 0; i < products.length; i++) {
@@ -26,4 +57,17 @@ function printProducts(products) {
     }
 }
 products.sort(compareSold);
+console.log('Products sorted by sold: ')
+printProducts(products);
+
+products.sort(compareName);
+console.log('Products sorted by name: ')
+printProducts(products);
+
+products.sort(compareCalories);
+console.log('Products sorted by calories: ')
+printProducts(products);
+
+products.sort(compareColor);
+console.log('Products sorted by color: ')
 printProducts(products);
