3fd39435923b645dc25de9d4cb8ce6ad16e33ccb
Dima <77278863+dpolevodin@users.noreply.github.com>
2021-05-19 12:40:24 +0500

    Update Objects_builder.js

--- Objects_builder.js
+++ Objects_builder.js
@@ -6,10 +6,15 @@ function Dog(name, breed, weight) {
     this.weight = weight;
 }
 
-function Duck(type, canFly) {
-    this.type = type;
-    this.canFly = canFly;
-}
-
 let fido = new Dog('Fido', 'Mixed', 38);
-alert(fido.breed);
+let fluffy = new Dog('Fluffy', 'Poodle', 30);
+let spot = new Dog('Spot', 'Chihuahua', 10);
+let dogs = [fido, fluffy, spot];
+
+for (let i = 0; i < dogs.length; i++) {
+    let size = 'small';
+    if (dogs[i].weight > 10) {
+        size = 'large';
+    }
+    console.log('Dog: ' + dogs[i].name + 'is a ' + size + ' ' + dogs[i].breed);
+}
