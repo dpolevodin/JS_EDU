ac3832c4a71ebb7cfa3f18431a9b8b90f7120cf9
Dima <77278863+dpolevodin@users.noreply.github.com>
2021-05-20 15:04:07 +0500

    Update Dog_obj_Ex.js

--- Dog_obj_Ex.js
+++ Dog_obj_Ex.js
@@ -31,8 +31,35 @@ Dog.prototype.sit = function() {
 let fido = new Dog("Fido", "Mixed", 38);
 let fluffy = new Dog("Fluffy", "Poodle", 30);
 let spot = new Dog("Spot", "Chihuahua", 10);
-
 let barnaby = new Dog('Barnaby', 'Basset Hound', 55);
-barnaby.sitting = true;
-barnaby.sit();
-spot.sit();
+
+let aDog = new Dog();
+
+function ShowDog(name, breed, weight, handler) {
+    Dog.call(this, name, breed, weight); //call — встроенный метод, который может использоваться с любой функцией (не забывайте, что Dog — это функция). 
+                                         //Dog.call вызывает функцию Dog и передает ей объект, 
+                                        //который должен использоваться как this, вместе со всеми аргументами функции Dog.
+    this.handler = handler;
+}
+
+ShowDog.prototype = new Dog();
+
+ShowDog.prototype.league = "Webville";
+ShowDog.prototype.stack = function() {
+console.log("Stack");
+};
+ShowDog.prototype.bait = function() {
+console.log("Bait");
+};
+ShowDog.prototype.gait = function(kind) {
+console.log(kind + "ing");
+};
+ShowDog.prototype.groom = function() {
+console.log("Groom");
+};
+
+let scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+scotty.stack();
+scotty.bark();
+console.log(scotty.league);
+console.log(scotty.species);
